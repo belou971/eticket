0e04d3f7202e4b95437176111350da675139289e
belou971 <belou971@users.noreply.github.com>
2017-10-20 23:26:25 +0200

    Add new formbuilder type AvailableDateType to generate the datetimepicker into the form.

---
+++ Symfony/src/EO/ETicketBundle/Form/AvailableDateType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace EO\ETicketBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+//use Symfony\Component\Form\Extension\Core\Type\DateType;
+//use EO\ETicketBundle\Form\DatePickerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AvailableDateType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date', DatePickerType::class, array());
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'EO\ETicketBundle\Entity\AvailableDate'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'eo_eticketbundle_availabledate';
+    }
+
+
+}
